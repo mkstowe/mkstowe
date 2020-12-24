bd8815a3de4bb377a153901d561d94064cbc880e
Michael Stowe <mkstowe@umich.edu>
2020-12-23 20:08:08 -0500

    Update

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -8,9 +8,10 @@ static_path = os.path.join(project_root, 'mkstowe/static')
 app = Flask(__name__, template_folder=template_path, static_folder=static_path)
 
 
-# @app.route('/')
-# def index():
-#     return 'Hello from flask'
+@app.route('/')
+def index():
+    """Display / route."""
+    return render_template("index.html")
 
 
 application = app
