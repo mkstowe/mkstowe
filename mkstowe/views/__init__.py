a157b6a46543672fed329d0e65cbc90b75b01739
Michael Stowe <mkstowe@umich.edu>
2020-12-24 17:36:32 -0500

    Updated views init

--- mkstowe/views/__init__.py
+++ mkstowe/views/__init__.py
@@ -1,6 +1,8 @@
 """Views, one for each page."""
 from mkstowe.views.index import index
 from mkstowe.views.skills import skills
+from mkstowe.views.projects import projects
+from mkstowe.views.experience import experience
 from mkstowe.views.resume import resume
 from mkstowe.views.code import code, metroid, timber, mission_o_possumble
 from mkstowe.views.living_arts import living_arts
