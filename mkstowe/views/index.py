b5899b174051e43abf724e5ccba3fa40a4011ba3
Michael Stowe <mkstowe@umich.edu>
2020-12-24 03:39:42 -0500

    Test with index

--- mkstowe/views/index.py
+++ mkstowe/views/index.py
@@ -10,7 +10,8 @@ import mkstowe
 @mkstowe.app.route('/', methods=['GET'])
 def index():
     """Display / route."""
-    return flask.render_template("index.html")
+    context = {"title": "Home", "desc": "Home/About page for Michael Stowe", "load_file": "index.html"}
+    return flask.render_template("master.html", **context)
 
 
 @mkstowe.app.route('/test/', methods=['GET'])
