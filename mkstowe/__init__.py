c01539e373f4ebb2f1638b556478f4d6e3f3e91d
Michael Stowe <mkstowe@umich.edu>
2021-03-09 21:51:51 -0500

    Global variables

--- mkstowe/__init__.py
+++ mkstowe/__init__.py
@@ -1,6 +1,8 @@
 """mkstowe.com package initializer."""
 import flask
 from datetime import datetime
+from gensim.models import Word2Vec
+import csv
 
 # app is a single object used by all the code modules in this package
 app = flask.Flask(__name__)  # pylint: disable=invalid-name
@@ -14,6 +16,24 @@ app.config.from_envvar('MKSTOWE_SETTINGS', silent=True)
 @app.context_processor
 def inject_now():
     return {'now': datetime.utcnow()}
+
+
+@app.context_processor
+def events_to_dict():
+    event_dict = {}
+    with open(app.config["STATIC_ROOT"]/'events.csv') as file:
+        for line in csv.DictReader(file, fieldnames=["story", "event_idx", "text"], quotechar='"', delimiter=',',
+                                   quoting=csv.QUOTE_ALL, skipinitialspace=True):
+            if line['story'] not in event_dict:
+                event_dict[line['story']] = [line['text']]
+            else:
+                event_dict[line['story']].append(line['text'])
+
+    return event_dict
+
+
+app.config["VARIEGATA_MODEL"] = Word2Vec.load(str(app.config["VARIEGATA_ROOT"]/'model'/'variegata.model'))
+app.config["VARIEGATA_EVENTS_LIST"] = events_to_dict()
 
 
 import mkstowe.views  # noqa: E402  pylint: disable=wrong-import-position
