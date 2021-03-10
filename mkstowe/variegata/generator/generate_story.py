e33a79cd383074c04f693be9b5bf212ebaed35d7
Michael Stowe <mkstowe@umich.edu>
2021-03-09 21:36:19 -0500

    Re enable generator again

--- mkstowe/variegata/generator/generate_story.py
+++ mkstowe/variegata/generator/generate_story.py
@@ -19,16 +19,16 @@ def events_to_dict():
     return event_dict
 
 
-# model = Word2Vec.load(str(mkstowe.app.config["VARIEGATA_ROOT"] / 'model' / 'variegata.model'))
-# events_dict = events_to_dict()
+model = Word2Vec.load(str(mkstowe.app.config["VARIEGATA_ROOT"] / 'model' / 'variegata.model'))
+events_dict = events_to_dict()
 
 
 def generate_story(num_nodes):
     story_events = []
-    # cur_node = str(random.choice(list(events_dict.keys()))) + "_0"
-    # for i in range(num_nodes):
-    #     cur_node = random.choice(model.most_similar(cur_node)[:10])[0]
-    #     split_node = cur_node.split("_")
-    #     story_events.append(events_dict[split_node[0]][int(split_node[1])])
+    cur_node = str(random.choice(list(events_dict.keys()))) + "_0"
+    for i in range(num_nodes):
+        cur_node = random.choice(model.most_similar(cur_node)[:10])[0]
+        split_node = cur_node.split("_")
+        story_events.append(events_dict[split_node[0]][int(split_node[1])])
 
     return story_events
